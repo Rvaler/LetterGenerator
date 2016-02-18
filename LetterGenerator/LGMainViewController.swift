bc457ef760c8225dd7c1620183909f2b34b2fe01
Rafael Valer <rvaler93@gmail.com>
2016-02-18 18:21:06 -0200

    ready

--- LetterGenerator/LGMainViewController.swift
+++ LetterGenerator/LGMainViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class LGMainViewController: UIViewController {
 
+    @IBOutlet weak var lblTimer: UILabel!
     @IBOutlet weak var lblLetter: UILabel!
     @IBOutlet weak var btnRandomizeLetter: UIButton!
+    @IBOutlet weak var btnPauseResume: UIButton!
     
     var loopControl = 0
+    var time = 120
+    var timerSeconds = NSTimer()
     var timerLetterChange = NSTimer()
+    var isPaused = true
     let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
         "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Z", "W", "Y"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.btnPauseResume.enabled = false
         // Do any additional setup after loading the view.
     }
 
@@ -30,10 +36,46 @@ class LGMainViewController: UIViewController {
     
     // MARK: - Actions
     
+    @IBAction func actionPauseResumeClicked(sender: AnyObject) {
+        if(isPaused) {
+            self.btnPauseResume.setTitle("Pause", forState: .Normal)
+            self.btnPauseResume.tintColor = UIColor.redColor()
+            isPaused = false
+            timerSeconds = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "updateTimer", userInfo: nil, repeats: true)
+        } else {
+            self.btnPauseResume.setTitle("Resume", forState: .Normal)
+            self.btnPauseResume.tintColor = UIColor.blueColor()
+            isPaused = true
+            self.timerSeconds.invalidate()
+        }
+    }
+    
     @IBAction func actionRandomizeLetterClicked(sender: AnyObject) {
         //26
+        self.timerSeconds.invalidate()
+        self.btnRandomizeLetter.enabled = false
+        self.btnPauseResume.enabled = true
+        self.isPaused = false
+        self.time = 120
+        self.calculateTime(time)
+        timerSeconds = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "updateTimer", userInfo: nil, repeats: true)
         timerLetterChange = NSTimer.scheduledTimerWithTimeInterval(0.05, target: self, selector: "updateLetter", userInfo: nil, repeats: true)
-        
+    }
+    
+    func calculateTime(time: Int){
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        self.lblTimer.text = String(format:"%02i:%02i", minutes, seconds)
+    }
+    
+    func updateTimer(){
+        if(self.time != 0) {
+            self.time--
+            self.calculateTime(self.time)
+        }else{
+            self.btnPauseResume.enabled = false
+            self.timerSeconds.invalidate()
+        }
     }
     
     func updateLetter(){
@@ -44,9 +86,10 @@ class LGMainViewController: UIViewController {
         }else{
             self.timerLetterChange.invalidate()
             self.loopControl = 0
+            self.btnRandomizeLetter.enabled = true
         }
     }
-
+    
     /*
     // MARK: - Navigation
 
